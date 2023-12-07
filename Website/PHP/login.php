653299d31d83f1380bee2b0490c88ea91f00fb7f
michaelbabinec <michaelbabinec@csu.fullerton.edu>
2023-12-07 10:56:33 -0800

    Update login.php

--- Website/PHP/login.php
+++ Website/PHP/login.php
@@ -3,19 +3,16 @@ if(session_status() !== PHP_SESSION_ACTIVE) session_start();
 
 include('connect.php');
 
-// Get user input
 $username = $_POST["username"];
 $password = $_POST["password"];
 
 //$hashed_pass = password_hash($_POST['password'], PASSWORD_DEFAULT); // Hash the password, this will be sent to DB
 
-    // Query to check if the username and password match
+
     $query = "SELECT * FROM users WHERE username='$username' AND passwrd='$password'";
     $result = mysqli_query($link, $query);
 
-    // Check if the query was successful
     if ($result) {
-        // Check if a matching user was found
         if (mysqli_num_rows($result) == 1) {
             $row = $result->fetch_assoc();
         
